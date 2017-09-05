2f3ca5b48ea1bbfdf7c14284927d20b3b9afa916
Renze de Vries <rdevries@ebay.com>
2017-09-05 12:03:57 +0200

    ISSUE35 Introduced a CorsFilter allowing cross origin requests to work for the new spring-mvc approach

---
+++ rest/jasdb_restservice/src/main/java/com/oberasoftware/jasdb/rest/service/CorsFilter.java
@@ -0,0 +1,23 @@
+package com.oberasoftware.jasdb.rest.service;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CorsFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        chain.doFilter(req, res);
+    }
+
+    public void init(FilterConfig filterConfig) {}
+
+    public void destroy() {}
+}
